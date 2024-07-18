9688ce0208fa6948608f9d3e8d3f21fd429ea04a
carlo.tabayag <carlo.tabayag@august99.com>
2024-07-18 19:46:05 +0800

    Removed primevue theme - Aura

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,6 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -17,11 +16,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .use(PrimeVue, {
-                theme: {
-                    preset: Aura
-                }
-            })
+            .use(PrimeVue, { unstyled: true })
             .mount(el);
     },
     progress: {
